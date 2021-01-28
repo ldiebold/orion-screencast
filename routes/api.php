854a5e975ff89616d747ba894a7c17433371526d
ldiebold <luke@ldiebold.com>
2021-01-28 21:55:37 +1100

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\PostsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['as' => 'api.'], function () {
+    Orion::resource('posts', PostsController::class);
+});
